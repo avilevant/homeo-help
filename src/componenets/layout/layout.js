6cc607b449b0e7e7d9e56e8d81ecc92a79b732d6
avi levant <avilevant@gmail.com>
2021-01-08 20:52:57 +0200

    adding navigation

--- src/componenets/layout/layout.js
+++ src/componenets/layout/layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './layout.css';
+import ToolBar from '../navigation/toolbar/toolbar';
 
 const layout =(props)=>(
     <div>
-    
+    <ToolBar/>
     <div>Toolbar,SideDrawer, Backdrop</div>
     <main className='content' >
     {props.children}
---
+++ src/componenets/navigation/toolbar/toolbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './toolbar.css';
+
+const ToolBar =(props)=>(
+    <header className='toolbar'>
+        <div>Menu</div>
+        <div>Logo</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default ToolBar;
